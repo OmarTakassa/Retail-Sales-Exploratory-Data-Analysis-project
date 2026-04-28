35cc4ba33c1958b3e212f3c132e79b750425ed86
Omar Takassa <146325782+OmarTakassa@users.noreply.github.com>
2026-04-28 15:54:08 +0200

    Add SQL script to explore database objects and columns

---
+++ sql_scripts/01_database_explo.sql
@@ -0,0 +1,8 @@
+-- Explore all objects in the database
+SELECT * FROM INFORMATION_SCHEMA.TABLES;
+
+-- Explore all Columns in the database
+SELECT 
+       * 
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'dim_customers'
